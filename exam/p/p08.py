c90992f48071c17c1044804e2684b6e505e22097
peggy0329 <90901271+peggy0329@users.noreply.github.com>
2022-12-24 10:48:58 +0800

    Update p08.py

--- exam/p/p08.py
+++ exam/p/p08.py
@@ -1,6 +1,18 @@
 def p08(n=12):
     output=None
     # ↓程式區域↓
+    while n%4==0:
+        n//=4
+    if n%8==7:   #因为7 最少由  1,2,4  三个数组成
+        return 4
 
+    for i in range(n+1):
+        temp=i*i
+        if temp<=n:
+            if int((n-temp)**0.5)**2 +temp==n:
+                return 1 +(0 if temp ==0 else 1)
+        else:
+            break
+    return 3
     # ↑程式區域↑
     return output
